bc7dcfe5818499731c3cbf956c9c0b95cf3791da
007vict <super_vg@bk.ru>
2019-04-10 13:30:41 +0300

    change admin settings

--- myshopbyexample/urls.py
+++ myshopbyexample/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
 from django.utils.translation import gettext_lazy as _
 urlpatterns = i18n_patterns(
-    path(_('admin/'), admin.site.urls),
+    path('admin/', admin.site.urls),
     path(_('cart/'), include('cart.urls', namespace='cart')),
     path(_('orders/'), include('orders.urls', namespace='orders')),
     path(_('payment/'), include('payment.urls', namespace='payment')),
